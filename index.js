d7e7cb974323221e3fd2820b09dbf1ad9044f1c7
Tim Hofman <tim@reachdigital.nl>
2019-11-11 12:24:45 +0100

    +: played around with data mocks
    To enable this set 'mockEntireSchema' to 'true' when initializing
    the Apollo server.
    
    Note: uses the npm package 'casual' to generate random data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = require('./src/schema');
+const casual = require('casual');
 
 const books = [
     {
@@ -53,7 +54,26 @@ const resolvers = {
     },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+/**
+ * Mocking test
+ * To use this make sure that mockEntireSchema is set to 'true'
+ */
+const mocks = {
+    Query: () => ({
+        getBooks: () => ([
+            {
+                id: casual.integer(1,10000),
+                title: casual.name,
+            },
+            {
+                id: casual.integer(1,10000),
+                title: casual.name,
+            }
+        ]),
+    })
+}
+
+const server = new ApolloServer({ typeDefs, resolvers, mocks, mockEntireSchema: false});
 server.listen(5000).then(( { url }) => {
     console.log(`Server ready at ${url}`);
 });
